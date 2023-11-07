af9f7c71b156f988569585c530bcbfbb795064a4
G00Z_G00Z <edygomezgg4@gmail.com>
2023-11-07 16:47:35 -0600

    Continous curve

--- bezier_extern_lib.py
+++ bezier_extern_lib.py
@@ -1,24 +1,44 @@
-import bezier
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import bezier
 
-# Define the nodes of the Bezier curve
-nodes = np.asfortranarray(
+# Define the nodes for the first quadratic Bezier curve
+nodes1 = np.asfortranarray(
     [
-        [0.0, 0.2, 0.4, 0.6, 0.8, 1.0],  # x-coordinates
-        [0.0, 0.5, 0.3, 0.6, 0.2, 0.5],  # y-coordinates
+        [0.0, 0.5, 1.0],  # x-coordinates
+        [0.0, 1.0, 0.0],  # y-coordinates
     ]
 )
 
-# Create a Bezier curve from the nodes
-curve = bezier.Curve(nodes, degree=3)
+# Create the first Bezier curve
+curve1 = bezier.Curve(nodes1, degree=2)
 
-# Evaluate points on the curve
+# Define the nodes for the second quadratic Bezier curve
+# The first point (1.0, 0.0) is the same as the last point of the first curve
+# The second point (1.5, -1.0) is chosen to ensure the gradient is continuous
+# The third point (2.0, 0.0) is the end point of the second curve
+nodes2 = np.asfortranarray(
+    [
+        [1.0, 1.5, 2.0],  # x-coordinates
+        [0.0, -1.0, 0.0],  # y-coordinates
+    ]
+)
+
+# Create the second Bezier curve
+curve2 = bezier.Curve(nodes2, degree=2)
+
+# Plot both curves
 s_vals = np.linspace(0, 1, 100)
-points = curve.evaluate_multi(s_vals)
+points1 = curve1.evaluate_multi(s_vals)
+points2 = curve2.evaluate_multi(s_vals)
 
-# Plot the curve
-plt.plot(points[0, :], points[1, :], label="Bezier Curve")
-plt.scatter(nodes[0, :], nodes[1, :], color="red", label="Control Points")
+plt.plot(points1[0, :], points1[1, :], label="First Bezier Curve")
+plt.plot(points2[0, :], points2[1, :], label="Second Bezier Curve")
+plt.scatter(
+    nodes1[0, :], nodes1[1, :], color="red", label="Control Points of First Curve"
+)
+plt.scatter(
+    nodes2[0, :], nodes2[1, :], color="green", label="Control Points of Second Curve"
+)
 plt.legend()
 plt.show()
