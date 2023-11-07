55550132f9dbbf44edb11a585a31e04e40a40af1
G00Z_G00Z <edygomezgg4@gmail.com>
2023-11-07 15:24:11 -0600

    Sampled points

--- main.py
+++ main.py
@@ -1,48 +1,75 @@
 import cv2
 import numpy as np
 
-# Initialize a list to store the points
-image = "./images/sample-classroom.jpeg"
-points = []
+points = []  # List to store points
+image_path = "./images/sample-classroom.jpeg"
 
 
-# This function will be called whenever a mouse event happens
+def sample_points(start, end, interval=10):
+    """Sample points at fixed intervals along the line from start to end."""
+    sampled_points = []
+
+    # Calculate the distance between the start and end points
+    distance = np.linalg.norm(np.array(end) - np.array(start))
+
+    # Calculate the number of intervals to sample
+    num_samples = int(distance / interval)
+
+    # Calculate the vector going from start to end
+    vector = np.array(end) - np.array(start)
+
+    # Normalize the vector
+    unit_vector = vector / distance
+
+    # Sample points at interval distances
+    for i in range(1, num_samples):
+        point = np.array(start) + unit_vector * i * interval
+        sampled_points.append(point.astype(int))
+
+    return sampled_points
+
+
 def draw_circle(event, x, y, flags, param):
     global points
     if event == cv2.EVENT_LBUTTONDOWN:
-        # Draw a circle where the click happened
+        # Draw a circle at the clicked point
         cv2.circle(img, (x, y), 3, (0, 255, 0), -1)
 
-        # Append the point
+        # Append the new point
         points.append((x, y))
 
-        # Draw line if there are at least two points
+        # If we have at least two points, draw the connecting line
         if len(points) >= 2:
             cv2.line(img, points[-2], points[-1], (255, 0, 0), 2)
 
+            # Get the sampled points and draw them
+            sampled_points = sample_points(points[-2], points[-1])
+            for sp in sampled_points:
+                cv2.circle(img, tuple(sp), 2, (0, 0, 255), -1)
+
+        # Refresh the image
         cv2.imshow("image", img)
 
 
 # Load an image
-img = cv2.imread(image)
-
-# Check if the image was loaded correctly
+img = cv2.imread(image_path)
 if img is None:
     print("Error: could not load image")
     exit()
 
-# Create a window
 cv2.namedWindow("image")
-
-# Set the mouse callback function to `draw_circle`
 cv2.setMouseCallback("image", draw_circle)
 
-# Show the image and wait for a key press
+# Display the image and wait for a key press
 cv2.imshow("image", img)
 cv2.waitKey(0)
 
-# Save the resulting image
-cv2.imwrite("./exports/image_with_lines.jpg", img)
+# Save the image with lines and sampled points
+cv2.imwrite("./exports/image_with_lines_and_samples.jpg", img)
 
-# Close all windows
 cv2.destroyAllWindows()
+
+# If you want to print out the sampled points for each line
+for i in range(1, len(points)):
+    sampled_points = sample_points(points[i - 1], points[i])
+    print(f"Sampled points between {points[i-1]} and {points[i]}: {sampled_points}")
