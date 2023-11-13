10aea4c06415e458033132c6e1064ba497c7f0e2
G00Z_G00Z <edygomezgg4@gmail.com>
2023-11-13 11:38:52 -0600

    Bezier curve

--- bezier_extern_lib.py
+++ bezier_extern_lib.py
@@ -1,56 +1,58 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import bezier
-
-
-def calculate_control_point(P1, P2, scale=1):
-    # Vector from P1 to P2
-    v = np.array(P2) - np.array(P1)
-    # P3 should be on the line from P1 through P2, we scale the vector if needed
-    P3 = np.array(P2) + scale * v
-    return P3.tolist()
-
-
-# Define the nodes for the first quadratic Bezier curve
-nodes1 = np.asfortranarray(
-    [
-        [0.0, 4.0, 6.0],  # x-coordinates
-        [0.0, 7.0, 3.0],  # y-coordinates
-    ]
+from bezier_lib.bezier_calc import (
+    calculate_bezier_points,
+    calculate_bezier_points_and_angles,
+    Point_T,
+    BezierPoints_T,
+    calculate_ctr_point,
+    points_to_bezier,
 )
 
-# Create the first Bezier curve
-curve1 = bezier.Curve(nodes1, degree=2)
 
-# The end point of the first curve and start of the second curve
-P2 = nodes1[:, -1]
+# Random waypoints for generating curves
+waypoints: list[Point_T] = [
+    (0, 0),
+    (1, 1),
+    (2, 0),
+    (3, 1),
+    (4, 0),
+    (5, 1),
+    (6, 0),
+    (7, 1),
+]
 
-# Define the end control point of the second curve
-P4 = [9.0, 10.0]
 
-# Calculate the control point P3 for the second Bezier curve
-P3 = calculate_control_point(nodes1[:, 1], P2)
+curves: list[bezier.Curve] = []
 
-# Define the nodes for the second quadratic Bezier curve
-nodes2 = np.asfortranarray(
-    [[P2[0], P3[0], P4[0]], [P2[1], P3[1], P4[1]]]  # x-coordinates  # y-coordinates
-)
+for i in range(1, len(waypoints)):
+    p1 = waypoints[i - 1]
+    p3 = waypoints[i]
+    ctr = calculate_ctr_point(p1, p3)
+    curve = points_to_bezier(p1, ctr, p3)
+    curves.append(curve)
 
-# Create the second Bezier curve
-curve2 = bezier.Curve(nodes2, degree=2)
 
 # Plot both curves
 s_vals = np.linspace(0, 1, 100)
-points1 = curve1.evaluate_multi(s_vals)
-points2 = curve2.evaluate_multi(s_vals)
 
-plt.plot(points1[0, :], points1[1, :], label="First Bezier Curve")
-plt.plot(points2[0, :], points2[1, :], label="Second Bezier Curve")
-plt.scatter(
-    nodes1[0, :], nodes1[1, :], color="red", label="Control Points of First Curve"
-)
-plt.scatter(
-    nodes2[0, :], nodes2[1, :], color="green", label="Control Points of Second Curve"
-)
+# Plot all the curves
+for curve in curves:
+    points = curve.evaluate_multi(s_vals)
+    plt.plot(points[0, :], points[1, :])
+
+
+# points1 = curve1.evaluate_multi(s_vals)
+# points2 = curve2.evaluate_multi(s_vals)
+
+# plt.plot(points1[0, :], points1[1, :], label="First Bezier Curve")
+# plt.plot(points2[0, :], points2[1, :], label="Second Bezier Curve")
+# plt.scatter(
+#     nodes1[0, :], nodes1[1, :], color="red", label="Control Points of First Curve"
+# )
+# plt.scatter(
+#     nodes2[0, :], nodes2[1, :], color="green", label="Control Points of Second Curve"
+# )
 plt.legend()
 plt.show()
