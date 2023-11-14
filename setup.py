29c4571325a696232c93eee9b622f4c84d3c9f76
G00Z_G00Z <edygomezgg4@gmail.com>
2023-11-14 10:28:11 -0600

    Added setup for installing

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+package_name = "bezier_lib"
+
+with open(f"./{package_name}/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name=package_name,
+    packages=find_packages(include=[package_name]),
+    version="0.0.1",
+    description="Bezier functions for calculating bezier points and angles",
+    long_description=long_description,
+    url="google.com",
+    long_description_content_type="text/markdown",
+    author="g00z-g00z",
+    license="MIT",
+    install_requires=["numpy", "bezier"],
+    test_requires=["pytest"],
+    extras_require={"dev": ["pytest", "twine"]},
+    setup_requires=["pytest-runner"],
+    test_suite="awesome_lib/tests",
+    python_requires=">=3.9",
+)
